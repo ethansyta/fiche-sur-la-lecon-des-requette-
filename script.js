bc551c2090be6e86160d2866c2a8dca12f079cdb
ethansyta <96329583+ethansyta@users.noreply.github.com>
2022-02-13 22:45:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+fetch(URL)
+.then(function(res){
+    if(res.ok){
+        return res.json
+    }
+
+})
+.then(function(value){
+    console.log(value)
+})
+
+.catch(function(err){
+
+})
+
+
+
+InputEvent.addEventListener(input, function(e)
+{
+    let result = e.target.value;
+    if(parametre){
+        isValid = true;
+    }else{
+        insValid = false;
+    }
+})
+
+
+function isValid(value)
+{
+    return /^e[0;9]{3,}
+}
+
+
+
+fetch(URL, {
+    methode: "POST ",
+
+header:{
+    'Accept': 'application/JSON'
+    'Content-type': 'application/JSON'
+},
+
+    body : json.stingify(JSONbody)
+});
